82f04d6f701992da630b6ab73f60706a8b475a80
Abhi-18-ai <ay414187@gmail.com>
2026-07-18 13:02:58 +0530

    triangle pattern added

--- Pattern/triangle.py
+++ Pattern/triangle.py
@@ -1,2 +1,16 @@
-# print a triangle
+# print a  right triangle
+"""
+*
+**
+***
+****
+*****
+"""
+n=5
+num = n
+for i in range(1,num+1):
+    for j in range(i):
+        print("*",end="")
+    print()
+
 
