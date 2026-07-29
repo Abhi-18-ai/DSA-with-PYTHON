01066ba6b463f0f9cfa5e620872d30c61db383d1
Abhishek Kumar <ay414187@gmail.com>
2026-07-29 17:17:34 +0530

    linear search question added

--- Search Algo/Linear Search/Count how many times 5 appears.py
+++ Search Algo/Linear Search/Count how many times 5 appears.py
@@ -1,3 +1,4 @@
+# how may times 5 apperears
 arr = [1,5,3,4,5,3,7,3,9,5,1,6,5]
 target = 5
 count = 0
@@ -6,3 +7,22 @@ for i in range(len(arr)):
         count = count+1
 
 print(count)
+
+
+
+# Find First Occurrence
+
+def first_occurrence(arr, target):
+
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+
+    return -1
+
+
+arr = [7, 3, 7, 8, 7]
+target = 7
+
+result = first_occurrence(arr, target)
+print(result)
