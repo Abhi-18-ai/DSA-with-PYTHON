3b465ae54004c3c98f2d14eb82f5f0f107eeae23
Abhishek Kumar <ay414187@gmail.com>
2026-09-25 18:43:36 +0530

    another added

--- Pattern/triangle.py
+++ Pattern/triangle.py
@@ -41,3 +41,66 @@ for i in range(1,n+1):
     for j in range(i):
         print("*",end="")
     print()
+
+
+# Pyramid
+''' 
+    *
+   ***
+  *****
+ *******
+*********
+'''
+n=5
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ",end="")
+    for j in range(2*i-1):
+        print("*",end="")
+    print()
+
+# Inverted Pyramid
+'''
+*********
+ *******
+  *****
+   ***
+    *
+'''
+n= 5
+for i in range(n):
+    for j in range(i):
+        print(" ",end="")
+    for j in range(2*(n-i)-1):
+            print("*",end="")
+    print()
+
+# Diamond Pattern
+'''
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+'''
+n = 5
+# Upper pyramid
+for i in range(1, n + 1):
+    for j in range(n - i):
+        print(" ", end="")
+    for j in range(2 * i - 1):
+        print("*", end="")
+    print()
+
+
+# Lower pyramid
+for i in range(1, n):
+    for j in range(i):
+        print(" ", end="")
+    for j in range(2 * (n - i) - 1):
+        print("*", end="")
+    print()
